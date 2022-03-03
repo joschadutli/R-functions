bfec14a72a700c33835a03496f0f23d4d3c26a25
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2022-03-03 17:44:23 +0100

    Bug fixed

--- PlotTools.R
+++ PlotTools.R
@@ -1,52 +1,67 @@
 
 
-summarySE <- function(.data, DV, between = NULL, within = NULL, ID = NULL, CI = 0.95){
-  
-  ### first aggregate the data on the level of participants
-  df <- .data %>% 
-    dplyr::group_by(across(c({{ ID }}, {{ between }}, {{ within }}))) %>%
-    dplyr::summarise(sub_DV = base::mean({{DV}}, na.rm=T)) %>% #When you have an env-variable that is a character vector
-    dplyr::ungroup()
-  
-  ### adjust the data to compute the within-subject se
-  if(!(missing(within))){
-    df <- df %>% 
-      dplyr::group_by(dplyr::across(c({{ ID }}, {{ between }}))) %>%
-      dplyr::mutate(user_mean = base::mean(sub_DV, na.rm = TRUE)) %>%
-      dplyr::ungroup() %>%
-      dplyr::group_by({{ between }}) %>%
-      dplyr::mutate(grand_mean = base::mean(sub_DV, na.rm = TRUE)) %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(DV_adjusted = sub_DV - user_mean + grand_mean)
-  } else{
-    df <- df %>% 
-      dplyr::mutate(DV_adjusted = sub_DV)
-  }
-  
-  ###aggregate data on level of plot
-  df <- df %>% 
-    dplyr::group_by(across(c({{ between }}, {{ within }}))) %>%
-    dplyr::summarize(
-      mean = base::mean(DV_adjusted, na.rm = TRUE),
-      n = dplyr::n(),
-      se = stats::sd(DV_adjusted, na.rm = TRUE)/base::sqrt(n),
-      ci = stats::qt(1-(1-CI)/2, n-1)*se,
-    ) %>%
-    dplyr::ungroup() %>% 
-    rename()
-  
-  ###add the Morey-correction to the se
-  if(!(missing(within))){
-    if(!(missing(between))){
-      df <- df %>% 
-        dplyr::group_by({{ between }}) %>%
-        dplyr::mutate(se = se * base::sqrt(dplyr::n()/(dplyr::n()-1))) %>%
-        dplyr::ungroup()
-    }else{
-      df <- df %>% 
-        dplyr::mutate(se = se * base::sqrt(dplyr::n()/(dplyr::n()-1)))
-    }}
+
+summarySE <-
+  function(.data,
+           DV,
+           between = NULL,
+           within = NULL,
+           ID = NULL,
+           CI = 0.95) {
+    ### first aggregate the data on the level of participants
+    df <- .data %>%
+      dplyr::group_by(dplyr::across(c({{ID}}, {{between}}, {{within}}))) %>%
+      dplyr::summarize(sub_DV = base::mean({{DV}}, na.rm = T)) %>% #When you have an env-variable that is a character vector
+      dplyr::ungroup()
+    
+    ### adjust the data to compute the within-subject se
+    if (!(is.null(within))) {
+      df <- df %>%
+        dplyr::group_by(dplyr::across(c({{ID}}, {{between}}))) %>%
+        dplyr::mutate(user_mean = base::mean(sub_DV, na.rm = TRUE)) %>%
+        dplyr::ungroup() %>%
+        dplyr::group_by(dplyr::across(c({{between}}))) %>%
+        dplyr::mutate(grand_mean = base::mean(sub_DV, na.rm = TRUE)) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(DV_adjusted = sub_DV - user_mean + grand_mean)
+    } else{
+      df <- df %>%
+        dplyr::mutate(DV_adjusted = sub_DV)
+    }
+    
+    ### aggregate data on level of plot
+    df <- df %>%
+      dplyr::group_by(dplyr::across(c({{between}}, {{within}}))) %>%
+      dplyr::summarize(
+        mean = base::mean(DV_adjusted, na.rm = TRUE),
+        n = dplyr::n(),
+        se = stats::sd(DV_adjusted, na.rm = TRUE) / base::sqrt(n),
+        ci = stats::qt(1 - (1 - CI) / 2, n - 1) * se,
+      ) %>%
+      dplyr::ungroup()
+    
+    
+    ### add the Morey-correction to the se
+    if (!(is.null(within))) {
+      if (!(is.null(between))) {
+        df <- df %>%
+          dplyr::group_by(dplyr::across(c({{between}}))) %>%
+          dplyr::mutate(
+            morey_correction = base::sqrt(dplyr::n() / (dplyr::n() - 1)),
+            se = se * morey_correction
+          ) %>%
+          dplyr::ungroup()
+      } else{
+        df <- df %>%
+          dplyr::mutate(
+            morey_correction = base::sqrt(dplyr::n() / (dplyr::n() - 1)),
+            se = se * morey_correction
+          )
+      }
+    }
     
     ### return df
+    df <- df %>%
+      select(!morey_correction)
     return(df)
   }
