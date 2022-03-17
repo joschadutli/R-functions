e41ac8de81cb9898d1ba23f86289bf3ce9a5ae56
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2022-03-17 12:14:27 +0100

    Create TidyFun.R

---
+++ TidyFun.R
@@ -0,0 +1,17 @@
+
+filterOutlier <- function(.data, vars, std=3){
+  df <- .data
+  for (var in vars) {
+    df <- df %>% 
+      mutate(zscore = scale(.data[[var]])) %>% 
+      mutate(across({{ var }}, .fns = ~ifelse(abs(zscore)>std,NA,.data[[var]]))) %>% 
+      mutate(across({{ var }}, .fns = ~ifelse(zscore >std, 
+                                              mean(.data[[var]],na.rm=T)+std*sd(.data[[var]],na.rm = T),
+                                              .data[[var]])),
+             across({{ var }}, .fns = ~ifelse(zscore < -std, 
+                                              mean(.data[[var]],na.rm=T)-std*sd(.data[[var]],na.rm = T),
+                                              .data[[var]]))) %>% 
+      select(!zscore)
+  }
+  return (df)
+}
