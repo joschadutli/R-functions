d6d91fbe278541b30830b1df47b1290c44af1e6c
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2023-02-02 18:05:25 +0100

    Update brmsFun.R

--- brmsFun.R
+++ brmsFun.R
@@ -1,7 +1,5 @@
 
 
-
-
 #' Combinations
 #'
 #' @param vars 
@@ -26,6 +24,180 @@ getFactComb <- function(vars) {
   
   # return all the factors
   return (factors) 
+}
+
+
+
+
+
+
+#' Calculating Bayes Factors
+#' 
+#' @author Chenyu Li
+#' 
+#' Credits to Henrik Singmann for providing the idea and template for a similar function implementation
+#' Credits to Philipp Musfeld for providing the initial verision of the function
+#' 
+#' This function runs several models and calculates Bayes factors.
+#' 1. The function creates a new data set before running the models.
+#' 2. In the data set, we create new columns for the interactions between/among variables.
+#' 3. The main reason to do this is to prevent the main effect is included into model when the interaction is in the model.
+#' 
+#'
+#' @param data the dataset which is used for fitting the model
+#' @param DV a character string stating how the dependent variable should be written in the formula.
+#' For most models, this is only naming the variable (e.g., "Reaction_Time"), 
+#' but for some models, it requires a more detailed descriptions 
+#' (e.g., for binomial models in brms: "n_correct | trials(n_trials)")
+#' @param fixEffects  a character vector specifying the fixed effects of the model
+#' @param randEffects a character vector naming the effects which should be tested / the alternative model formulas to create. Names must match effect 
+#' @param group a character vector naming a grouping variable for random effects 
+#' (currently only 1 grouping variable is supported, e.g. nesting effects within subject)
+#' @param by a character vector naming an additional variable, 
+#' if random effect structures should be estimated independently for different groups (e.g., for between-subject conditions)
+#' @param corr logical. Specifies whether correlations between effects should be estimated. Default is FALSE
+#' @param intercept 
+#' @param testEffects specified in fixed effects. Per default, this is set to the fixed effects, 
+#' meaning that all effects will be tested. If no effects need to be tested, set to NULL
+#' @param args a list of args that will input into brm function.
+#' @param path a character string. Where the models need to be saved.
+#' @param name a character string. The prefix of all the models
+#'
+#' @return a list including all to-be-test effects
+#' @export
+#'
+#' @examples
+cal_BF <-  function(
+    data, DV, fixEffects, randEffects, group, by = NULL, 
+    corr = FALSE, intercept = TRUE, testEffects = NULL, args, path, name) {
+  
+  
+  ### Default parameters
+  if (is.null(testEffects)) testEffects <- fixEffects
+  Int <- ifelse(intercept, 1, 0)
+  ### check if an additional grouping argument is provided:
+  by_text <- ifelse(is.null(by), "NULL", stringr::str_glue("c({stringr::str_c(by, collapse = ', ')})"))
+  testEffects = rev(testEffects) # to make sure that effects are tested from complex to simple, 
+  
+  
+  ### create the model fixed part of the model formula based on the passed arguments:
+  formula_fixed = reformulate(c(Int, fixEffects), intercept = TRUE)
+  
+  ### create the model matrix based on the data and the specified effects
+  model_matrix = model.matrix(formula_fixed, data)
+  
+  ### combine model matrix and data to include participant id and DV:
+  exl_cols <- unique(c(fixEffects, randEffects))
+  exl_cols <- exl_cols[exl_cols %in% names(data)]
+  new_data = data.frame(dplyr::select(data, !any_of(exl_cols)), model_matrix)
+
+  
+  
+  ### get the new names of the fixed effects from the model matrix
+  fixEffects_new = names(data.frame(model_matrix))[-1]
+
+    
+  ### if a random part of the model formula was specifyied, create this part of the model formula:
+  ### create the random part of the formula based on the new variable names:
+  ### select the correct column names based on the new effect names:
+  
+  
+  varName_match <- function(oldname, newVars) {
+    
+    # N-way interaction
+    N_inter = stringr::str_count(oldname, ":")
+    oldVars = stringr::str_split(oldname, ":")[[1]]
+    
+    output = c()
+    
+    # to check whether the new variable is a part of the old one.
+    for (i in 1:length(newVars)) {
+      
+      # N-way interaction
+      N_dot = stringr::str_count(newVars[i], "\\.")
+      if (N_dot != N_inter) next
+      
+      # Whether all the variables are included
+      matchCount = 0
+      for (a in 1: length(oldVars)) {
+        if (stringr::str_detect(newVars[i], oldVars[a])) matchCount = matchCount + 1
+      }
+      
+      # If yes, save the new variable
+      if (matchCount == length(oldVars)) output = c(output, newVars[i])
+    }
+    
+    return(output)
+  }
+  
+  ### create empty vector for the new random effect names:
+  randEffects_new = c()
+  
+  #### loop over the random effects
+  for (i in 1:length(randEffects)) {
+    new_rand <- varName_match(randEffects[i], fixEffects_new)
+    randEffects_new <- c(randEffects_new, new_rand)
+  }
+  
+  
+  # set default arguments for the functions ------------------------------------
+  
+  myModel <- function(fixFacts=NULL, randFacts=NULL, name) {
+    
+    fixForm <- str_c(c(intercept, fixFacts), collapse = " + ") 
+    randForm <- str_c(c(intercept, randFacts), collapse = " + ") 
+    
+    myForm <- as.formula(
+      stringr::str_glue("{DV} ~ {Int} + {fixForm} + (1 + {randForm} {bar} gr({group}, by = {by_text}, cor = {corr}))")
+      )
+    
+    new_args <- list(
+      formula = myForm,
+      file = stringr::str_glue("{path}{name}"),
+      data = new_data
+    )
+    
+    if (length(fixFacts)<1) {
+      input_args <- args
+      input_args$prior <- input_args$prior %>% filter(!class=="b")}
+    
+    do.call(brms::brm,args = append(new_args, args))
+  }
+  
+  # Full model -----------------------------------------------------------------
+  
+  fullModel <- myModel(fixEffects_new, randEffects_new, name = stringr::str_glue("{name}_full"))
+  
+  
+  # Output variable
+  outputBF <- list()
+  
+  # Fix effect -------------------------------------------
+  
+  
+  for (i in 1:length(testEffects)) {
+    
+    target <- testEffects[i]
+    excl_factors <- varName_match(target, fixEffects_new)
+    tmpFixFact <- fixEffects_new[!fixEffects_new %in% excl_factors]
+    model_name <- stringr::str_glue("{name}_noFix_{gsub(':', '-', target)}")
+    
+    # fit the model or read the model from the folder.
+    if (!file.exists(stringr::str_glue("{path}{model_name}"))) {
+      testModel <- myModel(tmpFixFact, randEffects_new, name = model_name)
+    } else {
+      testModel <- readRDS(stringr::str_glue("{path}{model_name}"))
+    }
+    
+    # which model is the best model
+    factBF <- brms::bayes_factor(fullModel, testModel)$bf
+    outputBF[stringr::str_glue("fix_{target}")] = factBF
+    
+  }
+  
+  return(outputBF)
+  
+    
 }
 
 
@@ -55,94 +227,6 @@ createForm <- function(DV, fixFact = NULL, randFact = NULL, ID = "subject", inte
   return(as.formula(str_glue("{DV} ~ {fixForm} + ({randForm} {bar} {ID})")))
 
 }
-
-
-
-#' Calculate Bayes Factor
-#'
-#' @param data An object of class data.frame (or one that can be coerced to that class) containing data of all variables used in the model.
-#' @param DV Dependent variable;
-#' @param fixFact Fixed factors;
-#' @param randFact Random factors;
-#' @param ID group variable;
-#' @param intercept Integer (0, 1); If 1, the intercept will be free estimated; If 0, the intercept will not be estimated.
-#' @param corr Logical;
-#' @param args A list of arguments that will input into the brm function.
-#' @param name The prefix of the models' name
-#' @param path The path where saved the models
-#'
-#' @return A list of Bayes factors
-#' @export
-#'
-#' @examples
-calculateBF <- function(data, DV, fixFact, randFact, testFact=NULL, ID, intercept = 1, corr= FALSE, args, name, path) {
-  
-  # set default arguments for the functions ------------------------------------
-  
-  myModel <- function(fixFact=NULL, randFact=NULL, name) {
-    
-    fixForm <- str_c(c(intercept, fixFact), collapse = " + ") 
-    randForm <- str_c(c(intercept, randFact), collapse = " + ") 
-    
-    bar <- ifelse(corr, "|", "||")
-    
-    myForm <- as.formula(str_glue("{DV} ~ {fixForm} + ({randForm} {bar} {ID})"))
-    
-    new_args <- list(
-      formula = myForm,
-      file = stringr::str_glue("{path}{name}"),
-      data = data
-    )
-    
-    if (length(fixFact)<1) {
-      input_args <- args
-      input_args$prior <- input_args$prior %>% filter(!class=="b")}
-    
-    do.call(brms::brm,args = append(new_args, args))
-  }
-  
-  # Full model -----------------------------------------------------------------
-  
-  fullModel <- myModel(fixFact, randFact, name = stringr::str_glue("{name}_full"))
-  
-  
-  # Output variable
-  outputBF <- list()
-  
-  
-  # Fix effect -------------------------------------------
-  
-  if (is.null(testFact)) testFact <- fixFact
-  
-  
-  for (i in 1:length(testFact)) {
-    
-    target <- fixFact[length(fixFact)+1-i]
-    tmpFixFact <- fixFact[!fixFact==target]
-    model_name <- stringr::str_glue("{name}_noFix-{gsub(':', '-', target)}")
-    
-    # fit the model or read the model from the folder.
-    if (!file.exists(stringr::str_glue("{path}{model_name}"))) {
-      testModel <- myModel(tmpFixFact, randFact, name = model_name)
-    } else {
-      testModel <- readRDS(stringr::str_glue("{path}{model_name}"))
-    }
-    
-    # which model is the best model
-    factBF <- brms::bayes_factor(fullModel, testModel)$bf
-    outputBF[stringr::str_glue("fix_{target}")] = factBF
-    
-  }
-  
-  return(outputBF)
-  
-}
-
-
-
-
-
-
 
 
 
