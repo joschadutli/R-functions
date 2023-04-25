ee3d9feac1f3e725210740051df14287b0dcb544
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2023-04-25 13:36:03 +0200

    Access to JATOS

--- TidyFun.R
+++ TidyFun.R
@@ -1,10 +1,81 @@
 
 
+#' Download data from JATOS
+#' 
+#' This function downloads result data from a JATOS server using an API token and specified UUID and batch ID.
+#' It then unzips and extracts the relevant data, including metadata, file names, and file contents.
+#' The function returns a data frame with the relevant data.
+#'
+#' @param token An API token, you can create a new one on JATOS. Details see: https://www.jatos.org/JATOS-API.html#personal-access-tokens
+#' @param url Server address. The default value is the address of our lab server.
+#' @param uuId A unique code of study, you can find it on JATOS
+#' @param batchId A unique code of a batch session in the study.
+#' @param dataPath A path used to save data. If NUll, data will be saved in working directory.
+#'
+#' @return data.frame
+#' @export
+#'
+get_JATOS_data <- function(token, url = "https://coglab.xyz/jatos/api/v1/results", uuId, batchId, dataPath = NULL) {
+  
+  require(httr) # Load the httr library
+  
+  if (is.null(dataPath)) dataPath = "./" # Set the default data path
+  
+  # Create the authorization header with the specified token
+  headers = headers = c(`Authorization` = str_glue("Bearer {token}"))
+  
+  # Send an HTTP GET request to the specified URL, passing in the UUID and batch ID
+  # Also pass in the authorization header and write the response to a temporary file
+  res <- httr::GET(
+    url = str_glue("{url}?studyUuid={uuId}&batchId={batchId}"),
+    httr::add_headers(.headers=headers), 
+    write_disk(str_glue("{dataPath}tmp.jrzip"))
+  )
+  
+  # Unzip the downloaded file and extract the file names into a list
+  filelist = unzip(str_glue("{dataPath}tmp.jrzip"), exdir=str_glue("{dataPath}JATOS_DATA"))
+  
+  # Extract relevant data from the file names and store in a data frame
+  file_table <- data.frame(filelist) %>% 
+    rename(file = filelist) %>% 
+    mutate(resultID = str_extract(file,"study_result_([0-9]+)"),
+           componentID = str_extract(file,"comp-result_([0-9]+)"),
+           resultID = as.numeric(str_extract(resultID,"([0-9]+)")),
+           componentID = as.numeric(str_extract(componentID,"([0-9]+)")))
+  
+  # Remove the temporary file
+  file.remove(str_glue("{dataPath}tmp.jrzip"))
+  
+  # Read the metadata from the last file in the list (assuming it is in JSON format)
+  metaData <- read_json(filelist[length(filelist)])$data[[1]]$studyResults
+  
+  # Extract relevant metadata from the metadata list and store in a data frame
+  info_table <- data.frame()
+  for (a in 1:length(metaData)) {
+    info_table[a, "resultID"] = metaData[[a]]$id
+    info_table[a, "componentID"] = metaData[[a]]$componentResults[[1]]$id
+    info_table[a, "duration"] = ifelse(is.null(metaData[[a]]$duration),
+                                       metaData[[a]]$lastSeenDate - metaData[[a]]$startDate,
+                                       sapply(strsplit(metaData[[a]]$duration, ":"), function(n) as.numeric(n) %*% c(3600, 60, 1))
+    )
+    info_table[a, "studyState"] = metaData[[a]]$studyState
+  }
+  
+  # Combine the file data and metadata into a single data frame
+  outcome <- info_table %>% 
+    inner_join(file_table) %>% 
+    filter(!is.na(resultID))
+  
+  # Return the combined data frame
+  return(outcome)
+}
+
+
 
 filterOutlier <- function(.data, vars, std = 3) {
-  df <- .data
+
   for (var in vars) {
-    df <- df %>%
+    df <- .data %>%
       mutate(zscore = scale(.data[[var]])) %>%
       mutate(across({{var}}, .fns = ~ ifelse(abs(zscore) > std, NA, .data[[var]]))) %>%
       mutate(across({{var}}, .fns = ~ ifelse(
