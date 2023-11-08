a7ddae7af103fde7e32fc6bb0b2012c6b8cb85af
Chenyu <57704488+chenyu-psy@users.noreply.github.com>
2023-11-08 10:28:59 +0100

    upload mutate_fun

---
+++ after_model/mutate_fun.R
@@ -0,0 +1,36 @@
+#'Create a new column according to the input function
+#'
+#'@param .data a data frame
+#'@param formulas a formula variable, the formula use to calculate the value of new variable.
+#'The dependent variable will be the name of the new column.
+#'@return a data frame with new column
+#'
+mutate_fun <- function(.data, formulas) {
+  
+  if (require(dplyr)) install.packages("dplyr")
+  if (require(stringr)) install.packages("stringr")
+  
+  # copy the data frame
+  results <- .data
+  
+  # convert formulas to list
+  formulas = c(formulas)
+  
+  # calculate the formula one by one
+  for (i in 1:length(formulas)) {
+    
+    # get the formula
+    form = formulas[[i]]
+    
+    # extract dependent variable
+    y = all.vars(form)[1]
+    # extract right-side formula
+    right_from = as.character(form[-2]) %>% 
+      stringr::str_remove("~")
+    # add the dependent variable to the data frame
+    results[, y] = with(results, eval(parse(text = right_from)))
+  }
+  
+  return(results)
+  
+}
